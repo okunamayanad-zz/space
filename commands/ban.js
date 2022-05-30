6493c2b10432c118025a1f7f5739fa5601f9f4bc
Baran Mordoğan <77172909+okunamayanad@users.noreply.github.com>
2022-05-30 19:22:19 +0300

    feat: :sparkles: Ban command!

---
+++ commands/ban.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { owners } = require('./config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Kişi banlayın.')
+        .addUserOption(
+            option =>
+                option
+                    .setName('target')
+                    .setDescription('Banlanacak kişiyi seçin.')
+                    .require(true))
+        .addStringOption(option =>
+            option
+                .setName('reason')
+                .setDescription('Ban sebebini girin.')
+                .require(false)),
+    async execute(interaction) {
+        const reason = interaction.getOption('reason');
+        const user = interaction.options.getUser('target');
+        if (!user) return interaction.reply('Kişi seçmedin.');
+        if (user.id === interaction.message.author.id) return interaction.reply('Kendini banlayamazsın.');
+        if (user.id === interaction.client.user.id) return interaction.reply('Beni banlayamazsın.');
+
+        owners.forEach(owner => {
+            if (user.id === owner) return interaction.reply('Sahibimi banlayamazın.');
+        });
+
+        // check if the user has the permissions to use this command
+        if (!interaction.message.member.hasPermission('BAN_MEMBERS')) return interaction.reply('Bu komutu kullanmak için yetkin yok.');
+        if (!interaction.message.guild.me.hasPermission('BAN_MEMBERS')) return interaction.reply('Hata! Bu komutu kullanmak için botun "BAN" yetkisi olması gerekiyor.');
+
+        // Check if the user is already banned
+        const bans = await interaction.client.database.bans.find({
+            guild: interaction.message.guild.id,
+            user: user.id
+        });
+        if (bans.length > 0) return interaction.reply('Kişi zaten banlı.');
+        // Check if the user has a higher role than the bot
+        if (user.roles.highest.position >= interaction.message.member.roles.highest.position) return interaction.reply('Kişi yetkiliyi yada daha yüksek bir rolü taşıyamazsın.');
+        // Ban the user
+        await interaction.message.guild.members.ban(user, {
+            reason: reason
+        });
+    },
+};
